5a879961886e613c59ec3620a2c22b86a7d4ae87
Jim Olesen <jim.e.olesen@gmail.com>
2022-09-18 19:40:46 +0200

    make function for closing modal, removes the entire element from the dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+
 function modal() {
-    console.log("helloworld")
+    body.innerHTML += `
+    <div id="modal-window"><div id="modal-box">
+    <p id="modal-contact">
+    Sorry, this does not exist yet.
+    If you want to book a session or buy other art, please use the info down below to contact me. <br>
+    Email: Almen-Ink@gmail.com <br>
+    Phone: 070-123 45 67
+    </p>
+    <span onclick="close" id="close-modal">X</span>
+    </div></div>
+    `;
+}
+
+function closeModal() {
+    document.getElementById("modal-window");
+    modal.remove();
 }
