4a436f3774bed73587a84ca15a072a127e29c908
Jim Olesen <jim.e.olesen@gmail.com>
2022-09-18 21:14:01 +0200

    restructured modal()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
 function modal() {
-    document.getElementById("body");
+    document.getElementsByTagName("body");
     body.innerHTML += `
     <div id="modal-window"><div id="modal-box">
     <p id="modal-contact">
