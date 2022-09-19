66a34cac010d53f32de433d326e0263c70c65cd8
Jim Olesen <jim.e.olesen@gmail.com>
2022-09-19 02:45:19 +0200

    added span to act as a closing button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 
 /**
  * Opens modal with contact info, when pressing link to webshop 
- * Or trying to manipulate the bookingform
+ * Or when trying to manipulate the bookingform
  */
 function openModal() {
     let modal = document.getElementById("modal-window");
     modal.style.display = "block";
     modal.innerHTML += `<div id="modal-box">
-                            <span id="close-modal" style="property:value;">X</span>
+                            <span class="close-modal" onclick="closeModal">X</span>
                         </div>`;
+}
+
+function closeModal() {
+    modal.style.display = "none";
 }
