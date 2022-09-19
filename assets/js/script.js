781fd14719e084248cd657020d11222b43672f84
Jim Olesen <jim.e.olesen@gmail.com>
2022-09-19 04:11:52 +0200

    add openModal() to webshop links and booking form, remove openModal() from input fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
-
+let modal = document.getElementById("modal-window");
 /**
  * Opens modal with contact info, when pressing link to webshop 
  * Or when trying to manipulate the bookingform
  */
 function openModal() {
-    let modal = document.getElementById("modal-window");
     modal.style.display = "block";
     modal.innerHTML += `<div id="modal-box">
-                            <span style="color: black" class="close-modal" onclick="closeModal">X</span>
+                            <span class="close-modal" onclick="closeModal()">X</span>
+                            <h2 style="font-size: 3rem">Ooops..</h2>
+                            <p style="font-size: 1.5rem; margin-left:5%; margin-right:5%; line-height: 1.5">Looks like this is broken, ask my LAZY web developer why that is :)<br>
+                            In the meantime, i am reachable in any of the following ways: <br>
+                            Phone: 070-123 45 67<br>
+                            OR <br>
+                            Almen-Ink@gmail.com
+                            </p>
                         </div>`;
 }
 
